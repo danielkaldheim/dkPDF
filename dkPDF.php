0bb90b74bb1c52b9a9210764f74a6e0c67dde627
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2013-12-18 17:27:25 +0100

    Added fill on row

--- dkPDF.php
+++ dkPDF.php
@@ -159,7 +159,7 @@ class dkPDF extends fpdf\FPDF {
 			}
 
 			//Print the text
-			$this->MultiCell($w, 5, $data[$i], 0, $a);
+			$this->MultiCell($w, 5, $data[$i], 0, $a, $fill);
 			//Put the position to the right of the cell
 			$this->SetXY($x + $w, $y);
 		}
